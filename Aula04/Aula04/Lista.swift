6942f98e1b1c7ac6c980754150c6f67798d62d01
Gabriel Nascimento <190107111@aluno.unb.br>
2025-06-04 10:45:26 -0300

    Update

--- Aula04/Aula04/Lista.swift
+++ Aula04/Aula04/Lista.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct Lista: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            NavigationLink(destination: item1){
+                Text("Indo para item 1")
+            }
+        }
     }
 }
 
---
+++ Aula04/Aula04/item1.swift
@@ -0,0 +1,18 @@
+//
+//  item1.swift
+//  Aula04
+//
+//  Created by Gabriel on 02/06/25.
+//
+
+import SwiftUI
+
+struct item1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    item1()
+}
